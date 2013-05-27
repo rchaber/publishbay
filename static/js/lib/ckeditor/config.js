a10e980f02a5ce850754e5e8f449dbb946f716a1
Richard Haber Home <rchaber@gmail.com>
2013-05-26 22:22:14 -0300

    more work on wysiwyg in new manuscript page

--- static/js/lib/ckeditor/config.js
+++ static/js/lib/ckeditor/config.js
@@ -10,25 +10,34 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
+		// { name: 'links' },
 		{ name: 'insert' },
-		{ name: 'forms' },
+		// { name: 'forms' },
+		// '/',
 		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		// '/',
 		{ name: 'others' },
-		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		'/',
 		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		{ name: 'colors' }
 	];
+
+	config.height='500px';
 
 	// Remove some buttons, provided by the standard plugins, which we don't
 	// need to have in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Print,Preview,Templates,Strike,Blockquote,Subscript,Superscript,Image,Table,BGColor,TextColor,Flash,Smiley,Iframe,PageBreak,Save,NewPage,DocProps,BidiLtr,BidiRtl,CreateDiv,ShowBlocks';
+
+	config.removePlugins = 'elementspath';
+
+	// config.uiColor = '#AADC6E';
+
+	config.skin = 'moonocolor';
 
 	// Se the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
